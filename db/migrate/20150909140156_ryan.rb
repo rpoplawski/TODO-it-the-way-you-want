06884d6819f7dfaf76b316aff3403b2100074834
Ryan Poplawski <claydewd@gmail.com>
2015-09-11 16:32:00 -0500

    initial commit

--- db/migrate/20150909140156_ryan.rb
+++ db/migrate/20150909140156_ryan.rb
@@ -1,4 +1,9 @@
 class Ryan < ActiveRecord::Migration
   def change
+  create_table   :todos do |t|
+      t.string   :name
+      t.string   :tasks
+      t.boolean  :completion_level
+    end
   end
 end
